388b9278b33ace9768744fe7191dc0c74708050b
= <=>
2024-07-29 11:55:09 +0300

    validators for the inputs added.

--- task_4/pkg/utils/flags.go
+++ task_4/pkg/utils/flags.go
@@ -1,26 +1,47 @@
 package utils
 
-import "flag"
+import (
+	"flag"
+	"log"
+	"strconv"
+)
 
 type Flags struct {
 	Shape  string
+	Radius float64
 	Width  float64
 	Height float64
-	Radius float64
 }
 
 func ParseFlags() Flags {
-	shape := flag.String("shape", "", "The shape to calculate the area for (circle or rectangle)")
-	width := flag.Float64("width", 0, "The width of the rectangle")
-	height := flag.Float64("height", 0, "The height of the rectangle")
-	radius := flag.Float64("radius", 0, "The radius of the circle")
+	var shape string
+	var radius, width, height string
 
+	flag.StringVar(&shape, "shape", "", "The shape to calculate the area for (circle or rectangle)")
+	flag.StringVar(&radius, "radius", "0", "The radius of the circle")
+	flag.StringVar(&width, "width", "0", "The width of the rectangle")
+	flag.StringVar(&height, "height", "0", "The height of the rectangle")
 	flag.Parse()
 
+	radiusValue, err := strconv.ParseFloat(radius, 64)
+	if err != nil {
+		log.Fatalf("Invalid value for -radius: %s, - is should be type INT", radius)
+	}
+
+	widthValue, err := strconv.ParseFloat(width, 64)
+	if err != nil {
+		log.Fatalf("Invalid value for -width: %s, - is should be type INT", width)
+	}
+
+	heightValue, err := strconv.ParseFloat(height, 64)
+	if err != nil {
+		log.Fatalf("Invalid value for -height: %s, - is should be type INT", height)
+	}
+
 	return Flags{
-		Shape:  *shape,
-		Width:  *width,
-		Height: *height,
-		Radius: *radius,
+		Shape:  shape,
+		Radius: radiusValue,
+		Width:  widthValue,
+		Height: heightValue,
 	}
 }
