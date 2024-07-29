40831cb0cf5d2c51b5f7fce73cb90c682c763861
= <=>
2024-07-29 19:18:20 +0300

    logrus added.

---
+++ task_5/pkg/logging/logging.go
@@ -0,0 +1,76 @@
+package logging
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path"
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+)
+
+type writerHook struct {
+	Writer    []io.Writer
+	LogLevels []logrus.Level
+}
+
+func (hook *writerHook) Fire(entry *logrus.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
+	}
+
+	text := []byte(line)
+
+	for _, w := range hook.Writer {
+		if _, err := w.Write(text); err != nil {
+			panic(err)
+		}
+	}
+
+	return err
+}
+
+func (hook *writerHook) Levels() []logrus.Level {
+	return hook.LogLevels
+}
+
+var e *logrus.Entry
+
+type Logger struct {
+	*logrus.Entry
+}
+
+func GetLogger() *Logger {
+	return &Logger{e}
+}
+
+func (l *Logger) GetLoggerWithField(k string, v interface{}) *Logger {
+	return &Logger{l.WithField(k, v)}
+}
+
+func init() {
+	l := logrus.New()
+	l.SetReportCaller(true)
+	l.Formatter = &logrus.TextFormatter{
+		CallerPrettyfier: func(frame *runtime.Frame) (function, file string) {
+			filename := path.Base(frame.File)
+
+			return fmt.Sprintf("%s()", frame.Function), fmt.Sprintf("%s:%d", filename, frame.Line)
+		},
+		DisableColors: false,
+		FullTimestamp: true,
+	}
+
+	l.SetOutput(io.Discard)
+
+	l.AddHook(&writerHook{
+		Writer:    []io.Writer{os.Stdout},
+		LogLevels: logrus.AllLevels,
+	})
+
+	l.SetLevel(logrus.TraceLevel)
+
+	e = logrus.NewEntry(l)
+}
